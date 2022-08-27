113580fdfd3d779cff7bf3bd9a28f9431ed5ea7a
Andrea <90292543+amgallo1213@users.noreply.github.com>
2022-08-27 16:51:50 -0400

    adding api search to app

--- client/src/components/AllConcerts.js
+++ client/src/components/AllConcerts.js
@@ -5,6 +5,7 @@ import { MDBTable, MDBTableHead, MDBTableBody,
     MDBCard, MDBCardBody, MDBCardImage, MDBCardText, MDBInput, MDBBtn } from 'mdb-react-ui-kit';
 import "../App.css";
 import NavBar from './NavBar';
+import ConcertSearch from './Search';
 
 
 
@@ -40,56 +41,52 @@ const AllConcerts = (props) => {
     return(
         <div>
             <NavBar />
-            <div id="home-card" className="homeCard">
-                <MDBCard className='mb-3 Card' >
-                    <MDBCardImage overlay src='https://skuawk.com/skuawk-photos/music/eszter-biro.jpg' alt='...' />   
-                    <MDBCardBody>
-                        <MDBCardText className="Tagline" style={{fontSize: "2.75em"}}>My upcoming concerts</MDBCardText>
-                        <Link to={'/concerts/create'}>Add a concert</Link>
-                        <MDBTable hover className="Table">
-                            <MDBTableHead>
-                                <tr>
-                                    <th scope='col' style={{fontSize: "1.8em"}}>Band Name</th>
-                                    <th scope='col' style={{fontSize: "1.8em"}}>Venue</th>
-                                    <th scope='col' style={{fontSize: "1.8em"}}>Date</th>
-                                    <th scope='col' style={{fontSize: "1.8em"}}>Tickets Purchased</th>
-                                    <th scope='col' style={{fontSize: "1.8em"}}>Cancel Concert</th>
-                                    <th scope='col' style={{fontSize: "1.8em"}}>After the Concert</th>
-                                </tr>
-                            </MDBTableHead>
-                            <MDBTableBody>
 
-                            {
-                                concertList.map((concert, index) => (
+            <div id="homePage">
+                <div id="home-card" className="homeCard">
+                    <MDBCard className='mb-3 Card' >
+                        <MDBCardImage overlay src='https://skuawk.com/skuawk-photos/music/eszter-biro.jpg' alt='...' />   
+                        <MDBCardBody>
+                            <MDBCardText className="Tagline" style={{fontSize: "2.75em"}}>My upcoming concerts</MDBCardText>
+                            <Link to={'/concerts/create'}>Add a concert</Link>
+                            <MDBTable hover className="Table">
+                                <MDBTableHead>
+                                    <tr>
+                                        <th scope='col' style={{fontSize: "1.8em"}}>Band Name</th>
+                                        <th scope='col' style={{fontSize: "1.8em"}}>Venue</th>
+                                        <th scope='col' style={{fontSize: "1.8em"}}>Date</th>
+                                        <th scope='col' style={{fontSize: "1.8em"}}>Tickets Purchased</th>
+                                        <th scope='col' style={{fontSize: "1.8em"}}>Cancel Concert</th>
+                                        <th scope='col' style={{fontSize: "1.8em"}}>After the Concert</th>
+                                    </tr>
+                                </MDBTableHead>
+                                <MDBTableBody>
 
-                                <tr key={index} className="concert">
-                                    <td className="border-end" ><Link to={`/concerts/${concert._id}`} style={{color: '#4cc9f0'}}>{concert.bandName}</Link></td>
-                                    <td className="border-end">{concert.venue}</td>
-                                    <td className="border-end">{concert.date}</td>
-                                    <td className="border-end">{concert.ticketsPurchased}</td>
-                                    <td className="border-end"><MDBBtn color="link" onClick={()=>{deleteConcert(concert._id)}}>cancel concert</MDBBtn></td>
-                                    <td><Link to={"/concerts/past-concerts"} style={{color: '#4cc9f0'}}>send to the archives</Link></td>
-                                    
-                                </tr>
+                                {
+                                    concertList.map((concert, index) => (
 
-                                ))
-                            }
+                                    <tr key={index} className="concert">
+                                        <td className="border-end" ><Link to={`/concerts/${concert._id}`} style={{color: '#4cc9f0'}}>{concert.bandName}</Link></td>
+                                        <td className="border-end">{concert.venue}</td>
+                                        <td className="border-end">{concert.date}</td>
+                                        <td className="border-end">{concert.ticketsPurchased}</td>
+                                        <td className="border-end"><MDBBtn color="link" onClick={()=>{deleteConcert(concert._id)}}>cancel concert</MDBBtn></td>
+                                        <td><Link to={"/concerts/past-concerts"} style={{color: '#4cc9f0'}}>send to the archives</Link></td>
+                                        
+                                    </tr>
 
-                            </MDBTableBody>
-                        </MDBTable>
-                    </MDBCardBody>
-                </MDBCard>
+                                    ))
+                                }
 
-                <MDBCard>
-                    <MDBCardImage src='https://images.unsplash.com/photo-1583795484071-3c453e3a7c71?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8Y29uY2VydCUyMHN0YWdlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60' alt='...' position='top' />
-                    <MDBCardBody>
-                        <MDBCardText style={{fontSize: "2.75em"}}>
-                            Search for Concerts
-                            <MDBInput label='Band Name' id='search' type='text' size="sm" />
-                            <MDBBtn outline rounded className='mx-2' color='dark'>Search</MDBBtn>
-                        </MDBCardText>
-                    </MDBCardBody>
-                </MDBCard>
+                                </MDBTableBody>
+                            </MDBTable>
+                        </MDBCardBody>
+                    </MDBCard>
+                </div>
+
+                <div id="concertSearch">
+                    <ConcertSearch />
+                </div>
             </div>
         </div>
     )
